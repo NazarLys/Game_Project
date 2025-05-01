e344ba6c3792a29a65b8b597f237a018fefee601
NazarDouble <nazarkoval942@gmail.com>
2025-05-01 19:21:55 +0200

    new1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,20 @@
+import telegramIcon from "../assets/logo_share.png";
+import gmailIcon from "../assets/Gmail_Icon.png";
+
 function Footer() {
-    return (
-      <footer className="w-full h-12 bg-gray-200 flex justify-center items-center">
-        <div className="flex flex-col items-center">
-          <a href="https://t.me/your_telegram" target="_blank" rel="noopener noreferrer">
-            <img src="logo_share.png" alt="Telegram" className="h-5 mb-1" />
-          </a>
-          <a href="mailto:yourname@gmail.com">
-            <img src="Gmail_Icon.png" alt="Gmail" className="h-5" />
-          </a>
-        </div>
-      </footer>
-    );
-  }
-  
-  export default Footer;
+  return (
+    <footer className="w-full h-[5cm] bg-neutral-800 flex justify-end items-center">
+      <div className="flex flex-col items-end mr-6">
+        <a href="https://t.me/your_telegram" target="_blank" rel="noopener noreferrer">
+          <img src={telegramIcon} alt="Telegram" className="h-6 mb-3" />
+        </a>
+        <a href="mailto:yourname@gmail.com">
+          <img src={gmailIcon} alt="Gmail" className="h-6" />
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
   
