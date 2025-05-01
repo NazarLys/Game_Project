857845e953d1ddbbc464921f6459978cd9596271
NazarDouble <nazarkoval942@gmail.com>
2025-05-01 18:25:51 +0200

    ababab

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -31,16 +31,6 @@ function About() {
   return (
     <div className="bg-neutral-900 text-white min-h-screen">
       <Navbar lang={lang} setLang={setLang} />
-      <div className="flex justify-end px-6 pt-4">
-        <select
-          value={lang}
-          onChange={(e) => setLang(e.target.value)}
-          className="bg-neutral-800 text-white p-2 rounded"
-        >
-          <option value="en">English</option>
-          <option value="pl">Polski</option>
-        </select>
-      </div>
 
       <main className="mx-auto px-6 md:px-32 lg:px-64 space-y-10 py-10">
         <h1 className="text-5xl font-bold text-center">{text[lang].heading1}</h1>
