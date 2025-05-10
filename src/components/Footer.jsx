684fadc2b512f538117d135e9589238b834a02ce
NazarDouble <nazarkoval942@gmail.com>
2025-05-10 21:02:23 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import gmailIcon from "../assets/Gmail_Icon.png";
 
 function Footer() {
   return (
-    <footer className="w-full h-[5cm] bg-neutral-800 flex justify-end items-center">
+    <footer className="w-full h-[4cm] bg-neutral-800 flex justify-end items-center">
       <div className="flex flex-col items-end mr-6">
         <a href="https://t.me/your_telegram" target="_blank" rel="noopener noreferrer">
           <img src={telegramIcon} alt="Telegram" className="h-6 mb-3" />
