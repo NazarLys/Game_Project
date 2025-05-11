3fd1c462e25fd796b7bf73af2160f52c299a5a52
Tsurimi <nazarlys702@gmail.com>
2025-05-11 21:17:48 +0200

    game_headers

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   base: "/",
   plugins: [
     react(),
     tailwindcss(),
-  ],
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_headers',
+          dest: '.' 
+        }
+      ]
+    })
+  ]
 });
